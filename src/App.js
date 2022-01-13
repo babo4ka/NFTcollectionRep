54d0e3268144e6babc1eaf7ef2578633573978f6
Eugene <evgeha_ivanov_01@bk.ru>
2022-01-13 16:35:33 +0300

    начало

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Mycomp from './components/Mycomp';
+
+function App() {
+  return (
+    <div>
+      <Mycomp></Mycomp>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mycomp.js
@@ -0,0 +1,9 @@
+import './css/Mycomp.css'
+
+const Mycomp = () =>{
+    return(
+        <div className="comp">hello!!</div>
+    )
+}
+
+export default Mycomp;
