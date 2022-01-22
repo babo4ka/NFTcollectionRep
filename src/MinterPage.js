354a823f45a3b468a8a332d42838babf6060961f
Eugene <evgeha_ivanov_01@bk.ru>
2022-01-22 20:05:51 +0300

    22.01.2022 20:05

--- src/MinterPage.js
+++ src/MinterPage.js
@@ -3,6 +3,7 @@ import AdvertisementBoard from './components/AdvertisementBoard';
 import Examples from './components/Examples';
 
 import './MinterPage.scss';
+import SiteButton from './components/SiteButton';
 
 function MinterPage() {
 
@@ -18,9 +19,38 @@ function MinterPage() {
       </div>
 
       <h3 id="examples_txt">EXAMPLES OF COLLECTION</h3>
-      <hr id="line"></hr>
+      <hr id="line"/>
 
       <Examples/>
+
+
+      <div className="row welcomes">
+
+        <div className="col col-sm welcome_page" id="ads_order">
+
+            <h2>YOU CAN ORDER AN ADS</h2>
+            <br/>
+            <h3>1 month, 1 week or even 1 hour as much as you want</h3>
+            <br/>
+            <SiteButton cn="order_btn" text="READ MORE"></SiteButton>
+
+        </div>
+
+
+        <div className="col col-sm welcome_page" id="rebus_welc">
+
+            <h2>TRY TO GET SYM CHEEPER</h2>
+            <br/>
+            <h3 >Just solve one of proposed rebuses and get one SYM</h3>
+            <br/>
+            <SiteButton cn="order_btn" text="Let's try!"></SiteButton>
+
+        </div>
+      </div>
+
+      <div className="row">
+        <span className="col about">тут короче будет описание, может какое-то предупреждение и тд и все такое бла бла бла бла бл абла бл</span>
+      </div>
       </div>
   );
 }
