202f33c5defca0aca8f1e4e79234a75daad78098
Eugene <evgeha_ivanov_01@bk.ru>
2022-01-25 20:22:39 +0300

    20:22 25.01.2022

--- src/RebusesPage.js
+++ src/RebusesPage.js
@@ -5,11 +5,12 @@ const RebusesPage = ()=>{
     return(
         <div className="container">
             <div class="row btn_holder">
-                <SiteButton isLeading={true} page="/" text="Back to minter page" cn="col-6" id="back_to_minter_btn"></SiteButton>
+                {/* <SiteButton isLeading={true} page="/" text="Back to minter page" cn="col-6" id="back_to_minter_btn"></SiteButton> */}
+                <a href="/" className="col-sm-6" id="back_to_minter_btn">Back to minter page</a>
             </div>
     
             <div className="row text_holder">
-                <span id="rebuses_info_txt" className="col-6">тут будет инфа о ребусах)0)</span>
+                <span id="rebuses_info_txt" className="col-sm-6">тут будет инфа о ребусах)0)</span>
             </div>
         </div>
     )
