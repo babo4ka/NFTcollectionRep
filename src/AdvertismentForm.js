b90cbd99b9a42b3c05e4516f015bc8276d93903f
Eugene <evgeha_ivanov_01@bk.ru>
2022-02-05 12:28:09 +0300

    12:28 05.02.2022

--- src/AdvertismentForm.js
+++ src/AdvertismentForm.js
@@ -30,43 +30,58 @@ const AdvertismentForm = ()=>{
                     <div className="container">
                         <div className="row">
 
-                        <div className="col-md-4 description_holder holder">
-                            <p>You can put some description</p>
-                            <p>
-                            <textarea
-                            maxLength={150} 
-                            placeholder='Put your description(150 symbols max)'
-                            id="description_area"
-                            ></textarea>
-                            </p>
-                        </div>
-
-                        <div className="col-md-4 duration_holder holder">
-                            <p>Choose show duration</p>
-
-                            <select class="form-select" aria-label="Default select example">
-                                <option selected>Hours</option>
-                                <option value="1">12 hours</option>
-                                <option value="2">Days</option>
-                                <option value="3">Weeks</option>
-                                <option value="4">Months</option>
-                            </select>
-
-                            <div className="row amount_btns">
-                                <SiteButton cn="amount_btn col-4 dur_btn" tn="btn_text_amount" text="-"></SiteButton>
-                                <h4 className="col-4">0</h4>
-                                <SiteButton cn="amount_btn col-4 dur_btn" tn="btn_text_amount" text="+"></SiteButton>
+                            <div className="col-md-4 description_holder holder">
+                                <p>You can put some description</p>
+                                <p>
+                                <textarea
+                                maxLength={150} 
+                                placeholder='Put your description(150 symbols max)'
+                                id="description_area"
+                                ></textarea>
+                                </p>
                             </div>
-                        
-                        </div>
 
-                        <div className="col-md-4 btns_holder holder">
-                            <div classname="row form_btns">
-                                <SiteButton cn="col-md-12 form_btn" tn="form_btn_text" id="reset_btn" text="Reset"></SiteButton>
-                                <SiteButton cn="col-md-12 form_btn" tn="form_btn_text" id="submit_btn" text="Submit"></SiteButton>
+                            <div className="col-md-4 duration_holder holder">
+                                <p>Choose show duration</p>
+
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Hours</option>
+                                    <option value="1">12 hours</option>
+                                    <option value="2">Days</option>
+                                    <option value="3">Weeks</option>
+                                    <option value="4">Months</option>
+                                </select>
+
+                                <div className="row amount_btns">
+                                    <SiteButton cn="amount_btn col-4 dur_btn" tn="btn_text_amount" text="-"></SiteButton>
+                                    <h4 className="col-4">0</h4>
+                                    <SiteButton cn="amount_btn col-4 dur_btn" tn="btn_text_amount" text="+"></SiteButton>
+                                </div>
                             </div>
+
+                            <div className="col-md-4 btns_holder holder">
+                                <div classname="row form_btns">
+                                    <SiteButton cn="col-md-12 form_btn" tn="form_btn_text" id="reset_btn" text="Reset"></SiteButton>
+                                    <SiteButton cn="col-md-12 form_btn" tn="form_btn_text" id="submit_btn" text="Submit"></SiteButton>
+                                </div>
+                            </div>
+
                         </div>
 
+                        <div className="row">
+                            <div className="col-12 col-lg-7 form_footer" id="price_text">
+                                <h5>Your price now is cost*time MATIC</h5>
+                            </div>
+
+                            <div className="col-12 col-lg-5 form_footer" id="add_btn_box">
+                                {/* <SiteButton id="add_btn" cn="" tn="add_btn_txt" text="Additional options"></SiteButton> */}
+                                <div className="container">
+                                    <div className="row" id="add_btn_block">
+                                        <div className="col-9 col-md-7 col-lg-9 text-truncate" id="add_txt">Additional options</div>
+                                        <div className="col-3 col-md-5 col-lg-3" id="add_icon">+</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
